1dc4955ec17b902cd14d080164ab822942614307
gmgunter23 <gmgunter23@students.bmchs.org>
2022-02-10 08:54:58 -0600

    Started the project today

---
+++ Decryption_Project.py
@@ -0,0 +1,7 @@
+username = input("What is your name? > ")
+
+answer = input(f"{username}, would you like to decrypt or encrypt?")
+
+message = input(f"{username}, what message would you like to {answer}")
+
+alphabet = 
