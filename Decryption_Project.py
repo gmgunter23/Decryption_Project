fbf7d86cd34b817ad0a08e8f6f76cf60003809b4
gmgunter23 <gmgunter23@students.bmchs.org>
2022-02-11 08:54:15 -0600

    didnt do much

--- Decryption_Project.py
+++ Decryption_Project.py
@@ -1,7 +1,10 @@
+from re import S
+
+
 username = input("What is your name? > ")
 
 answer = input(f"{username}, would you like to decrypt or encrypt?")
 
 message = input(f"{username}, what message would you like to {answer}")
 
-alphabet = 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
