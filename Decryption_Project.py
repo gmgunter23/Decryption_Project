5214ecab8c34d1f3686c000fbe767d25eb53d8f7
gmgunter23 <gmgunter23@students.bmchs.org>
2022-02-15 08:49:09 -0600

    finished projcet

--- Decryption_Project.py
+++ Decryption_Project.py
@@ -1,19 +1,48 @@
+from numpy import isin
+
 username = input("What is your name? > ")
 
-answer = input(f"{username}, would you like to decrypt or encrypt?")
+while True:
+    answer = input("Would you like to encrypt or decrypt? > ")
+    if answer == 'encrypt' or answer == 'decrypt':
+        break
+    else:
+        print("Please say either encrypt or decrypt.")
 
-message = input(f"{username}, what message would you like to {answer}")
+message = input(f"What message would you like to {answer} ").lower()
 
-key = input(f"{username}, what key would you like to cipher by?")
+while True:
+    key = input(f"{username}, what key would you like to cipher by? > ")
+    if key.isnumeric():
+        key = int(key)
+        break
+    else:
+        print("You need to have a number value please.")
 
-
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet = 'abcdefghijklmnopqrstuvwxyz '
 
 new_message = []
 
+if answer == 'encrypt':
+    for character in message:
+        for letter in alphabet:
+                if character == letter:
+                    letter_index = alphabet.index(letter) + key
+                    if letter_index >= 27:
+                        letter_index = letter_index - 27
+                        new_message.append(alphabet[letter_index])
+                    else:
+                        new_message.append(alphabet[letter_index])
+    print(''.join(new_message))
 
-for character in message:
-    for letter in alphabet:
-        if character == letter:
-            letter_index = alphabet.index(letter) + key
-            new_message.append(alphabet[letter_index])
+if answer == 'decrypt':
+    for character in message:
+        for letter in alphabet:
+                if character == letter:
+                    letter_index = alphabet.index(letter) - key
+                    if letter_index < 0:
+                        letter_index = letter_index + 27
+                        new_message.append(alphabet[letter_index])
+                    else:
+                        new_message.append(alphabet[letter_index])
+    print(''.join(new_message))
